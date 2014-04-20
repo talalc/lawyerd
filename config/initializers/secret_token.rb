3c4bbaa5cc1ff7e8e219366ddc018ce0856a7dfd
talalc <talalchoudry@gmail.com>
2014-04-20 13:05:02 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LawyerdApp::Application.config.secret_key_base = '9261ca53e1db9b3f5117ac03e2647efb64514c042a6aa9460dec2fa45f5068686893216658d32c0c64cb5d1ca42c23e3152a84d3973d4b01c844668e4da7d336'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LawyerdApp::Application.config.session_store :cookie_store, key: '_lawyerd_app_session'
