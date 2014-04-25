9c690022a305cc252f414d8c46bef5b57d67c3be
talalc <talalchoudry@gmail.com>
2014-04-25 11:35:10 -0400

    update & delete assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,4 +1,5 @@
 class AssignmentsController < ApplicationController
+
   def create
     @assignment = Assignment.new(assignment_params)
 
@@ -7,6 +8,35 @@ class AssignmentsController < ApplicationController
       redirect_to user_path(current_user)
     else
       flash[:notice] = "Invalid Assignment Properties"
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def edit
+    @assignment = Assignment.find(params[:id])
+    @clients = current_user.clients
+    @types = current_user.assignment_types
+  end
+
+  def update
+    @assignment = Assignment.find(params[:id])
+
+    if @assignment.update(assignment_params)
+      flash[:notice] = "Assignment updated"
+      redirect_to user_path(current_user)
+    else
+      flash[:notice] = "Invalid Assignment Properties"
+      render :edit
+    end
+  end
+
+  def destroy
+    assignment = Assignment.find_by(id: params[:id])
+    if assignment.destroy
+      flash[:notice] = "Assignment removed"
+      redirect_to user_path(current_user)
+    else
+      flash[:notice] = "Couldn't Remove Assignment"
       redirect_to user_path(current_user)
     end
   end
